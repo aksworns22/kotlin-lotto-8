fef075002735c6046658133e14151b687d425b5e
jangdaehan <wkdeogks17@gmail.com>
2025-11-01 04:20:26 +0900

    feat(Lotto): add duplicate number check

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,8 +2,14 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.size == LOTTO_SIZE) { "[ERROR] 로또 번호는 6개여야 합니다." }
     }
 
-    // TODO: 추가 기능 구현
+    init {
+        require(numbers.distinct().size == LOTTO_SIZE) { "[ERROR] 로또 번호는 중복되어서는 안됩니다." }
+    }
+
+    companion object {
+        const val LOTTO_SIZE = 6
+    }
 }
