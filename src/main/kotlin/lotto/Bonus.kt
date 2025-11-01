6a896fc311945237cd0f61257fbc17b67b6b134a
jangdaehan <wkdeogks17@gmail.com>
2025-11-01 17:30:42 +0900

    feat(bonus): add validation for invalid number

--- src/main/kotlin/lotto/Bonus.kt
+++ src/main/kotlin/lotto/Bonus.kt
@@ -1,4 +1,15 @@
 package lotto
 
 @JvmInline
-value class Bonus(val number: Int)
+value class Bonus(val number: Int) {
+    init {
+        require(isValidNumber(number)) { INVALID_NUMBER_ERROR }
+    }
+
+    companion object {
+        const val MIN_NUMBER = 1
+        const val MAX_NUMBER = 45
+        fun isValidNumber(number: Int) = number in MIN_NUMBER..MAX_NUMBER
+        const val INVALID_NUMBER_ERROR = "[ERROR] 올바른 보너스 번호 범위가 아닙니다."
+    }
+}
