cf8b93e716a0de3e0b3bb2180a95ccba496c1bdc
jangdaehan <wkdeogks17@gmail.com>
2025-11-01 20:54:40 +0900

    test(lottoPurchaseUnit): add test for negative money

--- src/test/kotlin/lotto/LottoPurchaseUnitTest.kt
+++ src/test/kotlin/lotto/LottoPurchaseUnitTest.kt
@@ -1,8 +1,12 @@
 package lotto
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
+import java.lang.IllegalArgumentException
 
 class LottoPurchaseUnitTest {
     @ParameterizedTest
@@ -11,5 +15,13 @@ class LottoPurchaseUnitTest {
         assertThat(LottoPurchaseUnit.from(money))
             .describedAs("올바른 금액 단위인지 확인하세요")
             .isEqualTo(LottoPurchaseUnit(numberOfLotto))
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [-1000, -1])
+    fun `구입 금액이 음수라면 예외 처리한다`(negativeMoney: Int) {
+        assertThatThrownBy { LottoPurchaseUnit.from(negativeMoney) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage(LottoPurchaseUnit.INVALID_MONEY_ERROR)
     }
 }
