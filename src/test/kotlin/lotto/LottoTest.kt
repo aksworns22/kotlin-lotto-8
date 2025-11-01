84a02e6c511dfca65a205be8917d51deb699b6ce
jangdaehan <wkdeogks17@gmail.com>
2025-11-01 16:21:56 +0900

    test(lotto): ensure that lotto size validation

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -4,6 +4,8 @@ import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.junit.jupiter.params.provider.ValueSource
 
 class LottoTest {
@@ -22,6 +24,14 @@ class LottoTest {
     }
 
     @ParameterizedTest
+    @MethodSource("invalidSizeNumbers")
+    fun `로또 번호의 개수를 유효한 개수만큼 뽑지 않으면 예외가 발생한다`(invalidSizeNumbers: List<Int>) {
+        assertThatThrownBy { Lotto(invalidSizeNumbers) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage(Lotto.LOTTO_SIZE_ERROR)
+    }
+
+    @ParameterizedTest
     @ValueSource(ints = [0, -1, 46])
     fun `로또 번호의 숫자 범위가 유효한 범위인지 확인한다`(illegalNumber: Int) {
         val validNumbers = arrayOf(1, 2, 3, 4, 5)
@@ -29,5 +39,15 @@ class LottoTest {
         assertThatThrownBy { Lotto(numbersWithInvalid) }
             .isInstanceOf(IllegalArgumentException::class.java)
             .hasMessage(Lotto.INVALID_LOTTO_NUMBER_ERROR)
+    }
+
+    companion object {
+        @JvmStatic
+        fun invalidSizeNumbers(): List<Arguments> {
+            return listOf(
+                Arguments.of(listOf(1, 2, 3, 4, 5)),
+                Arguments.of(listOf(1, 2, 3, 4, 5, 6, 7))
+            )
+        }
     }
 }
