0a5aa99e3253f83da1e7f920d7b091774e317e67
jangdaehan <wkdeogks17@gmail.com>
2025-11-02 00:09:03 +0900

    feat(purchaseRequest): add purchase message output

--- src/main/kotlin/lotto/PurchaseRequest.kt
+++ src/main/kotlin/lotto/PurchaseRequest.kt
@@ -6,9 +6,15 @@ object PurchaseRequest {
     fun from(userInput: UserInput): LottoPurchaseUnit {
         try {
             val money = userInput.readLine().trim().toDouble()
+            val lottoPurchaseUnit = LottoPurchaseUnit.from(money)
+            println("\n${lottoPurchaseUnit.count}개를 구매했습니다.")
             return LottoPurchaseUnit.from(money)
 
+        } catch (_: NumberFormatException) {
+            println(LottoPurchaseUnit.INVALID_MONEY_ERROR)
+            return from(userInput)
         } catch (error: IllegalArgumentException) {
+            println(error)
             return from(userInput)
         }
     }
