1bcefcb8f65850dbff27f5102f9d86cd97efee74
jangdaehan <wkdeogks17@gmail.com>
2025-11-01 20:55:01 +0900

    feat(lottoPurchaseUnit): add validation for negative money

--- src/main/kotlin/lotto/LottoPurchaseUnit.kt
+++ src/main/kotlin/lotto/LottoPurchaseUnit.kt
@@ -3,6 +3,11 @@ package lotto
 data class LottoPurchaseUnit(val count: Int) {
     companion object {
         const val VALID_MONEY_UNIT = 1000
-        fun from(money: Int) = LottoPurchaseUnit(money / VALID_MONEY_UNIT)
+        const val INVALID_MONEY_ERROR = "[ERROR] 유효하지 않은 구입 금액입니다."
+        fun from(money: Int): LottoPurchaseUnit {
+            require(money >= 0) { INVALID_MONEY_ERROR }
+            require(money % VALID_MONEY_UNIT == 0) { INVALID_MONEY_ERROR }
+            return LottoPurchaseUnit(money / VALID_MONEY_UNIT)
+        }
     }
 }
