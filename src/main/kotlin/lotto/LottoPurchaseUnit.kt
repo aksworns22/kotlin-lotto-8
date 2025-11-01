5c891652cc27e312ca9da69fabc54e5e77602ff8
jangdaehan <wkdeogks17@gmail.com>
2025-11-01 21:15:15 +0900

    feat(lottoPurchaseUnit): add validation for invalid floating point number

--- src/main/kotlin/lotto/LottoPurchaseUnit.kt
+++ src/main/kotlin/lotto/LottoPurchaseUnit.kt
@@ -9,5 +9,11 @@ data class LottoPurchaseUnit(val count: Int) {
             require(money % VALID_MONEY_UNIT == 0) { INVALID_MONEY_ERROR }
             return LottoPurchaseUnit(money / VALID_MONEY_UNIT)
         }
+
+        fun from(money: Double): LottoPurchaseUnit {
+            require(money >= 0) { INVALID_MONEY_ERROR }
+            require(money % VALID_MONEY_UNIT == 0.0) { INVALID_MONEY_ERROR }
+            return LottoPurchaseUnit((money / VALID_MONEY_UNIT).toInt())
+        }
     }
 }
