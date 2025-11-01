b4ce2240d3b4aa4b123fa04ee29e1a7ac59efcd5
jangdaehan <wkdeogks17@gmail.com>
2025-11-01 17:30:35 +0900

    test(bonus): validate that invalid numbers are used

---
+++ src/test/kotlin/lotto/BonusTest.kt
@@ -0,0 +1,15 @@
+package lotto
+
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class BonusTest {
+    @ParameterizedTest
+    @ValueSource(ints = [-1, 0, 46])
+    fun `보너스 번호의 숫자 범위가 유효한 범위인지 확인한다`(invalidNumber: Int) {
+        assertThatThrownBy { Bonus(invalidNumber) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage(Bonus.INVALID_NUMBER_ERROR)
+    }
+}
