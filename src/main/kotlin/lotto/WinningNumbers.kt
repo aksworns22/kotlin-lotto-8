d51190e7666c3ca1efd0c460b1ce62e489c5403e
jangdaehan <wkdeogks17@gmail.com>
2025-11-01 17:43:54 +0900

    feat(winningNumbers): add validation for duplicates between winning and bonus numbers

--- src/main/kotlin/lotto/WinningNumbers.kt
+++ src/main/kotlin/lotto/WinningNumbers.kt
@@ -6,13 +6,19 @@ data class WinningNumbers(val numbers: List<Int>, val bonus: Bonus) {
     }
 
     init {
-        require(numbers.distinct().size == SIZE_WITHOUT_BONUS) { DUPLICATED_ERROR }
+        val distinctNumbers = getDistinctNumbers()
+        require(distinctNumbers.size == SIZE_WITHOUT_BONUS) { DUPLICATED_ERROR }
+        require(!distinctNumbers.contains(bonus.number)) { DUPLICATED_WITH_BONUS_ERROR }
     }
 
     init {
         if (numbers.any { number -> !isValidNumber(number) }) {
             throw IllegalArgumentException(INVALID_NUMBER_ERROR)
         }
+    }
+
+    private fun getDistinctNumbers(): Set<Int> {
+        return numbers.toSet()
     }
 
     companion object {
@@ -22,6 +28,7 @@ data class WinningNumbers(val numbers: List<Int>, val bonus: Bonus) {
         const val INVALID_SIZE_ERROR = "[ERROR] 당첨 번호는 보너스 제외 ${SIZE_WITHOUT_BONUS}개여야 합니다."
         const val DUPLICATED_ERROR = "[ERROR] 당첨 번호는 중복되어서는 안됩니다."
         const val INVALID_NUMBER_ERROR = "[ERROR] 올바른 당첨 번호 범위가 아닙니다."
+        const val DUPLICATED_WITH_BONUS_ERROR = "[ERROR] 당첨 번호와 보너스는 중복되어서는 안됩니다."
         fun isValidNumber(number: Int) = number in MIN_NUMBER..MAX_NUMBER
     }
 }
