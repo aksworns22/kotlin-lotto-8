3cf5a9abd91a2784565f7e7f01864e6cf4ed978f
jangdaehan <wkdeogks17@gmail.com>
2025-11-01 23:40:24 +0900

    test(purchaseRequest): add test using user input to create LottoPurchaseUnit

---
+++ src/test/kotlin/lotto/PurchaseRequestTest.kt
@@ -0,0 +1,29 @@
+package lotto
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+class PurchaseRequestTest {
+    @ParameterizedTest
+    @MethodSource("userInput")
+    fun `사용자의 입력에 해당하는 만큼 구매 단위를 계산한다`(userInput: UserInput, numberOfLotto: LottoPurchaseUnit) {
+        assertThat(PurchaseRequest.from(userInput))
+            .isEqualTo(numberOfLotto)
+    }
+
+    companion object {
+        @JvmStatic
+        fun userInput(): List<Arguments> {
+            return listOf(
+                Arguments.of(FakeUserInput(listOf("1000")), LottoPurchaseUnit(1)),
+                Arguments.of(FakeUserInput(listOf("a", "1000")), LottoPurchaseUnit(1)),
+                Arguments.of(FakeUserInput(listOf("-1000", "-2000", "3000")), LottoPurchaseUnit(3)),
+                Arguments.of(FakeUserInput(listOf("1000.1", "2000.0")), LottoPurchaseUnit(2)),
+                Arguments.of(FakeUserInput(listOf("1000.0 ")), LottoPurchaseUnit(1)),
+                Arguments.of(FakeUserInput(listOf("100", "1000")), LottoPurchaseUnit(1))
+            )
+        }
+    }
+}
