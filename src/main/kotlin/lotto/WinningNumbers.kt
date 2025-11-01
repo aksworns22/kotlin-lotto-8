a858e4f9cf598bb66237e23caadaeba8a51f31cf
jangdaehan <wkdeogks17@gmail.com>
2025-11-01 17:18:20 +0900

    feat(winningNumbers): add validation for duplicated numbers

--- src/main/kotlin/lotto/WinningNumbers.kt
+++ src/main/kotlin/lotto/WinningNumbers.kt
@@ -1,14 +1,17 @@
 package lotto
 
-import lotto.Lotto.Companion.LOTTO_SIZE
-
 data class WinningNumbers(val numbers: List<Int>, val bonus: Bonus) {
     init {
         require(numbers.size == SIZE_WITHOUT_BONUS) { INVALID_SIZE_ERROR }
     }
 
+    init {
+        require(numbers.distinct().size == SIZE_WITHOUT_BONUS) { DUPLICATED_ERROR }
+    }
+
     companion object {
         const val SIZE_WITHOUT_BONUS = 6
         const val INVALID_SIZE_ERROR = "[ERROR] 당첨 번호는 보너스 제외 ${SIZE_WITHOUT_BONUS}개여야 합니다."
+        const val DUPLICATED_ERROR = "[ERROR] 당첨 번호는 중복되어서는 안됩니다."
     }
 }
