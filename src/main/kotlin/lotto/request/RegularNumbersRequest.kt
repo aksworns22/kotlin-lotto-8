32328a9a5ce6df790f582e216f13d1f1f0f0a8d0
jangdaehan <wkdeogks17@gmail.com>
2025-11-03 01:23:37 +0900

    style: remove unnecessary phrase

--- src/main/kotlin/lotto/request/RegularNumbersRequest.kt
+++ src/main/kotlin/lotto/request/RegularNumbersRequest.kt
@@ -8,7 +8,7 @@ object RegularNumbersRequest {
         while (true) {
             try {
                 println("당첨 번호를 입력해 주세요.")
-                return RegularNumbers.Companion.from(userInput.readLine().split(","))
+                return RegularNumbers.from(userInput.readLine().split(","))
             } catch (error: IllegalArgumentException) {
                 println(error.message)
             }
