9804e680b68642ea84a872bdb37a20e517b1a295
jangdaehan <wkdeogks17@gmail.com>
2025-11-02 19:16:54 +0900

    feat(application): run with real random numbers

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,7 +1,11 @@
 package lotto
 
 import lotto.util.ConsoleUserInput
+import lotto.util.UniqueRandomGenerator
 
 fun main() {
     val lottoPurchaseUnit = PurchaseRequest.from(ConsoleUserInput)
+
+    val boughtLotto = Lotto.from(UniqueRandomGenerator, lottoPurchaseUnit)
+    boughtLotto.print()
 }
