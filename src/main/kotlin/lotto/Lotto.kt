bea0cd8199c44aac666484a435da4420e3ffca3a
jangdaehan <wkdeogks17@gmail.com>
2025-11-02 15:58:49 +0900

    feat(lotto): implement random lotto generating

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,5 +1,7 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == LOTTO_SIZE) { LOTTO_SIZE_ERROR }
@@ -19,5 +21,11 @@ class Lotto(private val numbers: List<Int>) {
         const val LOTTO_SIZE = 6
         const val DUPLICATED_LOTTO_NUMBER_ERROR = "[ERROR] 로또 번호는 중복되어서는 안됩니다."
         const val LOTTO_SIZE_ERROR = "[ERROR] 로또 번호는 ${LOTTO_SIZE}개여야 합니다."
+        fun fromRandom(): Lotto {
+            val minNumber = LottoNumber.MIN_NUMBER
+            val maxNumber = LottoNumber.MAX_NUMBER
+            val randomNumbers = Randoms.pickUniqueNumbersInRange(minNumber, maxNumber, LOTTO_SIZE)
+            return Lotto(randomNumbers)
+        }
     }
 }
