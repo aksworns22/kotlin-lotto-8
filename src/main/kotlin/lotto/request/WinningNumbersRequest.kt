07c8d04469a9acfd4c058f07be08f20b0de39ef6
jangdaehan <wkdeogks17@gmail.com>
2025-11-03 01:23:01 +0900

    feat(winningNumbersRequest): handle duplication between regular and bonus numbers

---
+++ src/main/kotlin/lotto/request/WinningNumbersRequest.kt
@@ -0,0 +1,20 @@
+package lotto.request
+
+import lotto.Bonus
+import lotto.WinningNumbers
+import lotto.util.UserInput
+
+object WinningNumbersRequest {
+    fun from(userInput: UserInput): WinningNumbers {
+        val regularNumbers = RegularNumbersRequest.from(userInput)
+        while (true) {
+            try {
+                val bonusNumber = BonusNumberRequest.from(userInput)
+                return WinningNumbers(regularNumbers, bonusNumber)
+
+            } catch (error: IllegalArgumentException) {
+                println(error.message)
+            }
+        }
+    }
+}
