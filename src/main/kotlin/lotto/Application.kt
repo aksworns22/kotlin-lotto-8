77ed23c3468a381ef47a613e007cb1ee4a779fd0
jangdaehan <wkdeogks17@gmail.com>
2025-11-02 23:49:09 +0900

    feat(application): implement real console output about bonus number

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,6 @@
 package lotto
 
+import lotto.request.BonusNumberRequest
 import lotto.request.PurchaseRequest
 import lotto.request.RegularNumbersRequest
 import lotto.util.ConsoleUserInput
@@ -7,9 +8,8 @@ import lotto.util.UniqueRandomGenerator
 
 fun main() {
     val lottoPurchaseUnit = PurchaseRequest.from(ConsoleUserInput)
-
     val boughtLotto = Lotto.from(UniqueRandomGenerator, lottoPurchaseUnit)
     boughtLotto.print()
-
     val regularNumbers = RegularNumbersRequest.from(ConsoleUserInput)
+    val bonusNumber = BonusNumberRequest.from(ConsoleUserInput)
 }
