efc65db019889ca12826a2d47ecd7db2ba35bff5
jangdaehan <wkdeogks17@gmail.com>
2025-11-03 14:39:46 +0900

    feat(profit): display profit rate

--- src/main/kotlin/lotto/Profit.kt
+++ src/main/kotlin/lotto/Profit.kt
@@ -4,4 +4,8 @@ data class Profit(val rankResult: RankResult, val lottoPurchaseUnit: LottoPurcha
     val earnMoney = Rank.entries.sumOf { it.prize.money * rankResult.count[it]!! }
     val purchaseMoney = lottoPurchaseUnit.totalMoney
     val rate = earnMoney.toDouble() / purchaseMoney.toDouble()
+
+    fun printRate() {
+        println("총 수익률은 ${"%.1f".format(rate * 100)}%입니다.")
+    }
 }
