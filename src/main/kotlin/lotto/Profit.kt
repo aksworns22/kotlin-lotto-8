3e5a025a0e2c22641674179272d615398d6c04fb
jangdaehan <wkdeogks17@gmail.com>
2025-11-03 22:36:32 +0900

    style(profit): remove !! operator

--- src/main/kotlin/lotto/Profit.kt
+++ src/main/kotlin/lotto/Profit.kt
@@ -4,7 +4,7 @@ import lotto.result.PurchaseResult
 import lotto.result.RankResult
 
 data class Profit(val rankResult: RankResult, val purchaseResult: PurchaseResult) {
-    val earnMoney = Rank.entries.sumOf { rank -> rank.prize.money * rankResult.count[rank]!! }
+    val earnMoney = Rank.entries.sumOf { rank -> rank.prize.money * rankResult.count.getValue(rank) }
     val purchaseMoney = purchaseResult.totalMoney
     val rate = earnMoney.toDouble() / purchaseMoney.toDouble()
 
